af1de257535c7ab87fded4bcd4bbbf76a7ff359d
Tyrell C <tyrellrcurry@gmail.com>
2024-09-06 19:06:21 -0700

    first commit

---
+++ landingpage/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#9052F5",
+        "secondary": "#FF9C27",
+        "dark": "#181007",
+        "grey": "#808080",
+        "gradient": "#F4EEFF",
+        "btn-hover": "#FF900D"
+      }
+    },
+  },
+  plugins: [],
+}
+
